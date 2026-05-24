0d075aa3b185ac53f3e2b84050312fe0cd438681
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-05-24 03:17:04 -0500

    feat: add HealthCheck module and controller for gateway status monitoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdministrativeDataModule } from './administrative-data/administrative-data.module';
 import { UsersModule } from './users/users.module';
 import { TrajectoryModule } from './trajectory/trajectory.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-  imports: [AdministrativeDataModule, UsersModule, TrajectoryModule],
+  imports: [AdministrativeDataModule, UsersModule, TrajectoryModule, HealthCheckModule],
   controllers: [],
   providers: [],
 })
